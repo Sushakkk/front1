9f6720d3f48a7eb98ab436bc9143e43f1269970e
lexcf <lexcf@bk.ru>
2024-09-10 00:30:40 +0300

    finished main page and description

--- monitoring/threats/views.py
+++ monitoring/threats/views.py
@@ -3,13 +3,12 @@ import datetime
 
 # Create your views here.
 THREATS = [
-    {'id': 1, 'name': 'Запуск вредоносного ПО', 'short_description': 'Описание атаки 1', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'подробное описание атаки. многобукв. 123456787654548900','count':9,'price':10000},
-    {'id': 2, 'name': 'Подозрительная сетевая активность', 'short_description': 'Описание атаки 2', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'подробное описание атаки. многобукв. 123456787654548900','count':16,'price':10000},
-    {'id': 3, 'name': 'Исполнение скриптов PowerShell', 'short_description': 'Описание атаки 3', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'подробное описание атаки. многобукв. 123456787654548900','count':34,'price':10000},
-    {'id': 4, 'name': 'Эксплуатация известных уязвимостей', 'short_description': 'Описание атаки 4', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'подробное описание атаки. многобукв. 123456787654548900','count':9,'price':10000},
-    {'id': 5, 'name': 'Фишинговые атаки', 'short_description': 'Описание атаки 5', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900','count':12,'price':10000},
-    {'id': 6, 'name': 'Атаки DoS/DDoS', 'short_description': 'Описание атаки 6', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900','count':73,'price':10000},
-    {'id': 7, 'name': 'Эксплуатация уязвимостей web-приложений', 'short_description': 'Описание атаки 7', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900', 'count':1,'price':10000},
+    {'id': 1, 'name': 'Запуск вредоносного ПО', 'short_description': 'Мониторинг запуска вредоносного ПО', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'Наши специалисты детектируют запуск вредоносного программного обеспечения в режиме 24/7, что позволяет защитить рабочие станции пользователей','count':9,'price':10000},
+    {'id': 2, 'name': 'Сетевая активность', 'short_description': 'Мониторинг сетевой активности', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'Наши специалисты имеют большой опыт в детектировании атак, основываясь на анализе сетевого трафика','count':16,'price':10000},
+    {'id': 3, 'name': 'Эксплуатация уязвимостей', 'short_description': 'Мониторинг эксплуатации уязвимостей на рабочик станциях', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'подробное описание атаки. многобукв. 123456787654548900','count':9,'price':10000},
+    {'id': 4, 'name': 'Фишинговые атаки', 'short_description': 'Мониторинг проведения фишинговых атак', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'Наши системы обнаружения фишинга помогут эффективно обнаружить и предотвратить фишинговую атаку','count':12,'price':10000},
+    {'id': 5, 'name': 'Атаки DoS/DDoS', 'short_description': 'Мониторинг и противодействие DoS/DDoS атакам', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900','count':73,'price':10000},
+    {'id': 6, 'name': 'Эксплуатация уязвимостей web-приложений', 'short_description': 'Мониторинг эксплуатации уязвимостей web-приложений', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900', 'count':1,'price':10000},
 ]
 
 # TODO
@@ -24,8 +23,6 @@ def get_name_by_id(threats, search_id):
     return None
 
 def threats_list(request):
-    total_requests = 2 # TODO
-    
     threats = []
     if 'threat_name' in request.GET:
         for threat in THREATS:
@@ -41,8 +38,9 @@ def threat_description(request, id):
     return render(request, 'description.html', {'threat': data})
 
 def threat_request(request):
-    for threat in THREATS:
-        if threat["id"] == 3: # TODO
-            threat["status"] = 'test'
-            return render(request, 'request.html', {'current_threats':threat})
-    return render(request, 'request.html')
+
+    threat_ids = [request['threat_id'] for request in REQUESTS]
+    filtered_threats = [threat for threat in THREATS if threat['id'] in threat_ids]
+    for threat in filtered_threats:
+        threat["status"] = "Draft"
+    return render(request, 'request.html', {'current_threats':filtered_threats})
