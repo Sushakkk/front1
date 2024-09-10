082d6f398e60ac39344909d63469b95c7c5bed75
lexcf <lexcf@bk.ru>
2024-09-10 15:27:41 +0300

    refactor current requests page

--- monitoring/threats/views.py
+++ monitoring/threats/views.py
@@ -3,14 +3,13 @@ import datetime
 
 # Create your views here.
 THREATS = [
-    {'id': 1, 'name': 'Запуск вредоносного ПО', 'short_description': 'Мониторинг запуска вредоносного ПО', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'Наши специалисты детектируют запуск вредоносного программного обеспечения в режиме 24/7, что позволяет защитить рабочие станции пользователей','count':9,'price':10000},
-    {'id': 2, 'name': 'Сетевая активность', 'short_description': 'Мониторинг сетевой активности', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'Наши специалисты имеют большой опыт в детектировании атак, основываясь на анализе сетевого трафика','count':16,'price':18000},
-    {'id': 3, 'name': 'Эксплуатация уязвимостей', 'short_description': 'Мониторинг эксплуатации уязвимостей на рабочик станциях', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg', 'description':'подробное описание атаки. многобукв. 123456787654548900','count':9,'price':11900},
+    {'id': 1, 'name': 'Запуск вредоносного ПО', 'short_description': 'Мониторинг запуска вредоносного ПО', 'img_url':'http://127.0.0.1:9000/static/virus.jpg', 'description':'Наши специалисты детектируют запуск вредоносного программного обеспечения в режиме 24/7, что позволяет защитить рабочие станции пользователей','count':9,'price':10000},
+    {'id': 2, 'name': 'Сетевая активность', 'short_description': 'Мониторинг сетевой активности', 'img_url':'http://127.0.0.1:9000/static/network.jpg', 'description':'Наши специалисты имеют большой опыт в детектировании атак, основываясь на анализе сетевого трафика','count':16,'price':18000},
+    {'id': 3, 'name': 'Эксплуатация уязвимостей', 'short_description': 'Мониторинг эксплуатации уязвимостей на рабочик станциях', 'img_url':'http://127.0.0.1:9000/static/vulnerability.jpg', 'description':'Наши специалисты имеют большой опыт в детектировании эксплуатации известных уязвимостей и атак, таких как eternal blue и kerberoasting','count':9,'price':11900},
     {'id': 4, 'name': 'Фишинговые атаки', 'short_description': 'Мониторинг проведения фишинговых атак', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'Наши системы обнаружения фишинга помогут эффективно обнаружить и предотвратить фишинговую атаку','count':12,'price':10000},
-    {'id': 5, 'name': 'Атаки DoS/DDoS', 'short_description': 'Мониторинг и противодействие DoS/DDoS атакам', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900','count':73,'price':11500},
-    {'id': 6, 'name': 'Эксплуатация уязвимостей web-приложений', 'short_description': 'Мониторинг эксплуатации уязвимостей web-приложений', 'img_url':'http://127.0.0.1:9000/static/phishing.jpg','description':'подробное описание атаки. многобукв. 123456787654548900', 'count':7,'price':13200},
+    {'id': 5, 'name': 'Атаки DoS/DDoS', 'short_description': 'Мониторинг и противодействие DoS/DDoS атакам', 'img_url':'http://127.0.0.1:9000/static/ddos.jpg','description':'Наша система AntiDDoS успешно выявляет и блокирует DDoS атаки, что позволяет оставаться доступными для клиентов','count':73,'price':11500},
+    {'id': 6, 'name': 'Эксплуатация уязвимостей web-приложений', 'short_description': 'Мониторинг эксплуатации уязвимостей web-приложений', 'img_url':'http://127.0.0.1:9000/static/web.jpg','description':'Для проникновения внутрь компании хакеры зачастую используют уязвимости в web-приложений. Наши специалисты успешно детектируют и митигируют попытки эксплуатации таких уязвимостей', 'count':7,'price':13200},
 ]
-
 # TODO
 REQUESTS = [{'threat_id':4},{'threat_id':2},{'threat_id':6}]
 
@@ -42,5 +41,5 @@ def threat_request(request):
     threat_ids = [request['threat_id'] for request in REQUESTS]
     filtered_threats = [threat for threat in THREATS if threat['id'] in threat_ids]
     for threat in filtered_threats:
-        threat["status"] = "Draft"
+        threat["status"] = "Активно"
     return render(request, 'request.html', {'current_threats':filtered_threats})
